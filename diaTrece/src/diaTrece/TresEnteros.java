c7cc1c54425bf932274ae5ce62b6a728691229e5
homer <uribediazo@hotmail.com>
2023-06-21 23:30:10 -0600

    segundo ejercicio

---
+++ diaTrece/src/diaTrece/TresEnteros.java
@@ -0,0 +1,58 @@
+package diaTrece;
+
+import java.util.Scanner;
+
+public class TresEnteros {
+	
+	public static int valorUno() {
+		Scanner dato = new Scanner(System.in);
+		int valor1;
+		
+		System.out.print("Por favor ingrese el primer valor: ");
+		valor1 = dato.nextInt();
+		
+		return valor1;
+		
+	}
+	//valor dos
+	
+	public static int valorDos() {
+		Scanner dato = new Scanner(System.in);
+		int valor2;
+		
+		System.out.print("Por favor ingrese el segundo valor: ");
+		valor2 = dato.nextInt();
+		
+		return valor2;
+		
+	}
+	
+	// valor tres
+	
+	public static int valorTres() {
+		Scanner dato = new Scanner(System.in);
+		int valor3;
+		
+		System.out.print("Por favor ingrese el tercer valor: ");
+		valor3 = dato.nextInt();
+		
+		return valor3;
+		
+	}
+	
+	//tercero
+	
+	public static void main(String[] args) {
+		
+		int num1 = valorUno();
+		int num2 = valorDos();
+		int num3 = valorTres();
+		
+		int producto = num1 * num2 * num3;
+		
+		System.out.print("El producto de los numeros es: " + producto);
+		
+		
+	}
+
+}
